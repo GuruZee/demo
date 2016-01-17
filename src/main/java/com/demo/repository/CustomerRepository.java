1580de4c393ffb615c81adbfd5214ecf86f93393
Hadoop <hadoop@macbook-pro.home>
2016-01-17 16:19:05 +0000

    spring cloud demo
    spring cloud demo

---
+++ src/main/java/com/demo/repository/CustomerRepository.java
@@ -0,0 +1,16 @@
+package com.demo.repository;
+
+import java.util.List;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import com.demo.model.Customer;
+
+@RepositoryRestResource(collectionResourceRel = "customer", path = "customer")
+public interface CustomerRepository extends PagingAndSortingRepository<Customer, Long> {
+
+	List<Customer> findByLastName(@Param("name") String name);
+
+}
